931f4dc9ee67a56be1de30f5fb72b289413758e3
jp1924 <jsb10121249@gmail.com>
2025-01-21 12:20:22 +0900

    init

---
+++ src/main.py
@@ -0,0 +1,136 @@
+import json
+from dataclasses import dataclass, field
+from pathlib import Path
+
+from setproctitle import setproctitle
+
+from transformers import (
+    HfArgumentParser,
+    Seq2SeqTrainingArguments,
+    set_seed,
+)
+from transformers import logging as hf_logging
+from transformers.utils import is_sagemaker_mp_enabled
+
+
+@dataclass
+class CLSTrainingArguments(Seq2SeqTrainingArguments):
+    output_dir: str = field(
+        default=None,
+        metadata={"help": "The output directory where the model predictions and checkpoints will be written."},
+    )
+
+    def __post_init__(self):
+        if self.output_dir is None:
+            raise ValueError("output_dir은 무조건 설정되어 있어야 한다.")
+
+        super().__post_init__()
+
+        def _convert_str_dict(passed_value: dict):
+            "Safely checks that a passed value is a dictionary and converts any string values to their appropriate types."
+            for key, value in passed_value.items():
+                if isinstance(value, dict):
+                    passed_value[key] = _convert_str_dict(value)
+                elif isinstance(value, str):
+                    # First check for bool and convert
+                    if value.lower() in ("true", "false"):
+                        passed_value[key] = value.lower() == "true"
+                    # Check for digit
+                    elif value.isdigit():
+                        passed_value[key] = int(value)
+                    elif value.replace(".", "", 1).isdigit():
+                        passed_value[key] = float(value)
+
+            return passed_value
+
+        _ADDITIONAL_VALID_DICT_FILEDS = [
+            "data_truncate_map",
+            "data_name_map",
+            "config_kwargs",
+            "model_kwargs",
+            "tokenizer_kwargs",
+        ]
+        _VALID_LIST_FIELDS = [
+            "instruction_template",
+            "response_template",
+            "train_dataset_prefix",
+            "valid_dataset_prefix",
+            "test_dataset_prefix",
+            "freeze_named_param",
+        ]
+
+        # copied from: transformers/training_args.py/__post_init__()
+        for field in _ADDITIONAL_VALID_DICT_FILEDS:
+            passed_value = getattr(self, field)
+            # We only want to do this if the str starts with a bracket to indiciate a `dict`
+            # else its likely a filename if supported
+            if isinstance(passed_value, str) and passed_value.startswith("{"):
+                loaded_dict = json.loads(passed_value)
+                # Convert str values to types if applicable
+                loaded_dict = _convert_str_dict(loaded_dict)
+                setattr(self, field, loaded_dict)
+            elif isinstance(passed_value, dict) or passed_value is None:
+                pass
+            else:
+                raise ValueError(f"{field}은 dict로 설정해야 함.")
+
+        for field in _VALID_LIST_FIELDS:
+            passed_value = getattr(self, field)
+            if isinstance(passed_value, str) and passed_value.startswith("["):
+                loaded_list = json.loads(passed_value)
+                setattr(self, field, loaded_list)
+            elif isinstance(passed_value, list) or passed_value is None:
+                pass
+            else:
+                raise ValueError(f"{field}은 list로 설정해야 함.")
+
+        self.config_kwargs = {
+            **self.config_kwargs,
+            "attn_implementation": self.attn_implementation,
+        }
+
+        self.cache_dir = Path(self.cache_dir) if self.cache_dir else None
+        self.model_name_or_path = self.resume_from_checkpoint or self.model_name_or_path
+
+        if self.group_by_length:
+            logger.warning("group_by_length이 True임! loss계산에 영향을 끼칠 수 있으니 확인해.")
+
+    @property
+    def is_local_process_zero(self) -> bool:
+        return self.local_process_index == 0
+
+    @property
+    def is_world_process_zero(self) -> bool:
+        if is_sagemaker_mp_enabled():
+            import smdistributed.modelparallel.torch as smp  # type: ignore
+
+            return smp.rank() == 0
+        else:
+            return self.process_index == 0
+
+
+hf_logging.set_verbosity_info()
+logger = hf_logging.get_logger("transformers")
+
+
+def main(train_args: CLSTrainingArguments) -> None:
+    pass
+
+
+if "__main__" in __name__:
+    main()
+
+if "__main__" in __name__:
+    parser = HfArgumentParser([CLSTrainingArguments])
+    train_args, remain_args = parser.parse_args_into_dataclasses(return_remaining_strings=True)
+
+    if remain_args and train_args.is_world_process_zero:
+        logger.info(f"remain_args: {remain_args}")
+
+    if train_args.seed is not None:
+        set_seed(train_args.seed)
+
+    if train_args.run_name is not None:
+        setproctitle(train_args.run_name)
+
+    main(train_args)
